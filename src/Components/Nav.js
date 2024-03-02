20a41b1c34541c7a44b972687f9f6a1365cf6692
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-03-02 15:23:17 +0530

    feat: Create Nav component

---
+++ src/Components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import littlelemon_logo from "../images/littlelemon_logo.png";
+
+const Nav = () => {
+    return (
+        <nav>
+            <img src={littlelemon_logo} alt="Little Lemon Logo" ></img>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Menu</a></li>
+                <li><a href='#'>Reservations</a></li>
+                <li><a href='#'>Order Online</a></li>
+                <li><a href='#'>Login</a></li>
+
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Nav
