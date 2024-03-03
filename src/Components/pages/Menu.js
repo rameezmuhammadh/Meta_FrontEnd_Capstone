1ecf2b6552545ba42b2a38c1b51a907c05b9df8a
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-03-03 20:59:08 +0530

    Add menu page and components

---
+++ src/Components/pages/Menu.js
@@ -0,0 +1,10 @@
+import Heading from "../sections/menuPages/Heading";
+import MenuList from "../sections/menuPages/MenuList";
+export default function Menu() {
+  return (
+    <>
+      <Heading />
+      <MenuList />
+    </>
+  );
+}
---
+++ src/Components/sections/menuPages/MenuList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuImg from "../../../assets/menu.webp";
+
+export default function MenuList() {
+  return (
+    <div>
+         <img
+        className="menu-image"
+        src={MenuImg}
+        alt="Little Lemon Menu"
+      ></img>
+    </div>
+  )
+}
